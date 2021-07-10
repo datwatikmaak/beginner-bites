782c2f4126cf2e6dfc6a37c566c0edb3704fc1f0
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-10 05:53:25 +0200

    submission Bite 130 @ codechalleng.es

--- 130/cars.py
+++ 130/cars.py
@@ -12,20 +12,17 @@ with requests.Session() as s:
 def most_prolific_automaker(year):
     """Given year 'year' return the automaker that released
        the highest number of new car models"""
-    automakers = []
-    for d in data:
-        if d['year'] == year:
-            automakers.append(d['automaker'])
-
+    automakers = [d['automaker'] for d in data if d['year'] == year]
     return max(automakers, key=automakers.count)
 
 
 def get_models(automaker, year):
     """Filter cars 'data' by 'automaker' and 'year',
        return a set of models (a 'set' to avoid duplicate models)"""
-    models = []
-    for d in data:
-        if d['year'] == year and d['automaker'] == automaker:
-            models.append(d['model'])
+    models = [
+        d['model']
+        for d in data
+        if d['year'] == year and d['automaker'] == automaker
+    ]
 
     return set(models)
