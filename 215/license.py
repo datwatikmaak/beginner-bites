f0f1318362fb27ac1a212eea69c05f34755e4521
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-23 05:24:55 +0200

    submission Bite 215 @ codechalleng.es

--- 215/license.py
+++ 215/license.py
@@ -5,4 +5,8 @@ def validate_license(key: str) -> bool:
     """Write a regex that matches a PyBites license key
        (e.g. PB-U8N435EH-PG65PW87-IXPWQG5T-898XSZI4)
     """
-    pass
+    # pattern = "^PB(-[A-Z0-9]{8}){4}$"
+    # test_string = key
+    result = re.match("^PB(-[A-Z0-9]{8}){4}$", key)
+
+    return bool(result)
