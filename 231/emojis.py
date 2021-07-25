7dff941176ceff19d219559e3111a3e5abbb0a4d
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-25 08:27:13 +0200

    submission Bite 231 @ codechalleng.es

--- 231/emojis.py
+++ 231/emojis.py
@@ -8,4 +8,8 @@ IS_EMOJI = re.compile(r'[^\w\s,]')
 
 def get_emoji_indices(text: str) -> List[int]:
     """Given a text return indices of emoji characters"""
-    pass
+    result = IS_EMOJI.findall(text)
+
+    chars = [char for char in text]
+
+    return [index for index in range(len(chars)) if chars[index] in result]
