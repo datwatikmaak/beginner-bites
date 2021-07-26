31d677534bf9b2bdb9185b5c3e225e57ca460429
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-26 06:19:04 +0200

    submission Bite 238 @ codechalleng.es

--- 238/test_fibonacci.py
+++ 238/test_fibonacci.py
@@ -1,3 +1,14 @@
 from fibonacci import fib
+import pytest
+
 
 # write one or more pytest functions below, they need to start with test_
+@pytest.mark.parametrize("number, expected", [(2, 1), (4, 3), (10, 55)])
+def test_fib(number, expected) -> None:
+    assert fib(number) == expected
+
+
+# test error
+def test_fib_value_error() -> None:
+    with pytest.raises(ValueError):
+        assert fib(-1) == 1
