c8ab61b3dbe3e9cdd2cfb3293ca2287656f24fbe
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-30 06:16:31 +0200

    submission Bite 254 @ codechalleng.es

--- 254/scoping.py
+++ 254/scoping.py
@@ -5,4 +5,12 @@ def sum_numbers(numbers: list) -> int:
     """Sums passed in numbers returning the total, also
        update the global variable num_hundreds with the amount
        of times 100 fits in total"""
-    pass
+    global num_hundreds
+    total = sum(numbers)
+    if total >= 100:
+        hundreds = int(str(total)[:-2])
+        num_hundreds += hundreds
+    else:
+        num_hundreds = num_hundreds
+
+    return total
