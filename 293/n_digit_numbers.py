b6362fd8791d45a52e0de19bf10a228ca3242ecf
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-08-08 08:02:17 +0200

    keep on trying

--- 293/n_digit_numbers.py
+++ 293/n_digit_numbers.py
@@ -1,20 +1,7 @@
 from typing import List, TypeVar
-import math
 
 T = TypeVar('T', int, float)
 
 
 def n_digit_numbers(numbers: List[T], n: int) -> List[int]:
-    digits = []
-    for num in numbers:
-        if num > 0:
-            digits.append(int(math.log10(num)) + 1)
-        elif num == 0:
-            digits.append(1)
-        else:
-            digits.append(int(math.log10(-num)) + 2)  # +1 if you don't count the '-'
-
-    print(digits)
-
-
-n_digit_numbers([-1.1, 2.22, -3.333], 2)
+    pass
