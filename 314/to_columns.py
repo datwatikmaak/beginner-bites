36a9d57db7f362c06c3b41943592b0a5689a3917
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-08-09 09:40:05 +0200

    submission Bite 314 @ codechalleng.es

--- 314/to_columns.py
+++ 314/to_columns.py
@@ -2,4 +2,10 @@ from typing import List  # not needed when we upgrade to 3.9
 
 
 def print_names_to_columns(names: List[str], cols: int = 2) -> None:
-    pass
+    chunks = [names[i:i + cols] for i in range(0, len(names), cols)]
+
+    for row in chunks:
+        output = ["| " + row[0].ljust(10)]
+        for col in row[1:]:
+            output.append("| " + col.ljust(10))
+        print(''.join(output))
