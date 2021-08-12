f921c9b4051aa6beb223bc05bcd5f8282cebad20
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-08-12 05:33:25 +0200

    submission Bite 322 @ codechalleng.es

--- 322/progress.py
+++ 322/progress.py
@@ -3,4 +3,7 @@ from datetime import datetime
 
 def ontrack_reading(books_goal: int, books_read: int,
                     day_of_year: int = None) -> bool:
-    pass
+    if not day_of_year:
+        day_of_year = (datetime.today() - datetime(datetime.today().year, 1, 1)).days
+    days_goal = books_goal / 365
+    return (day_of_year * days_goal) <= books_read
