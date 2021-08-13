ba2190d36091a84a0090891858980e19d75d24af
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-08-13 05:52:45 +0200

    submission Bite 323 @ codechalleng.es

--- 323/intersection.py
+++ 323/intersection.py
@@ -1,6 +1,9 @@
-import functools
 from typing import Iterable, Set, Any
 
 
 def intersection(*args: Iterable) -> Set[Any]:
-    pass
+    # filter out the None and empty
+    filtered_args = list(filter(None, args))
+    if not filtered_args:
+        return set()
+    return set(filtered_args[0]).intersection(*filtered_args[1:])
