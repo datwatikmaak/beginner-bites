1e5ab7f193287f389a4a5d8994080ad84ea32711
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-08-17 02:24:40 +0200

    submission Bite 4 @ codechalleng.es

--- 4/tags.py
+++ 4/tags.py
@@ -1,10 +1,12 @@
 import os
+import re
 from collections import Counter
 import urllib.request
 import xml.etree.ElementTree as ET
 
 # prep
-tempfile = os.path.join('/tmp', 'feed')
+tmp = os.getenv("TMP", "/tmp")
+tempfile = os.path.join(tmp, 'feed')
 urllib.request.urlretrieve(
     'https://bites-data.s3.us-east-2.amazonaws.com/feed',
     tempfile
@@ -17,4 +19,12 @@ with open(tempfile) as f:
 def get_pybites_top_tags(n=10):
     """use Counter to get the top 10 PyBites tags from the feed
        data already loaded into the content variable"""
-    pass
+
+    # find all the categories with regex
+    categories = (re.findall("<category>(.*?)</category>", content, re.DOTALL))
+
+    # count the number of occurence of each category
+    top_tags = Counter(categories)
+
+    # return the n most common tags
+    return top_tags.most_common(n)
