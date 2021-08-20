5e51a3e1bca9f81a08ba77aea70104f66a5910bd
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-08-20 06:54:56 +0200

    submission Bite 10 @ codechalleng.es

--- 10/divide.py
+++ 10/divide.py
@@ -1,2 +1,16 @@
 def positive_divide(numerator, denominator):
-    pass
+
+    try:
+        result = numerator / denominator
+
+        if result < 0:
+            raise ValueError
+
+        if not isinstance(numerator, (int, float)) or not isinstance(denominator, (int, float)):
+            raise TypeError
+
+    except ZeroDivisionError:
+        return 0
+
+    else:
+        return result
