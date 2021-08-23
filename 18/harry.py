f6943b37642ca2cd692cff08405c4cbd55d21434
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-08-23 08:56:18 +0200

    Got the most frequent word, but number of frequency is wrong.

--- 18/harry.py
+++ 18/harry.py
@@ -1,5 +1,7 @@
 import os
 import urllib.request
+import string
+from collections import Counter
 
 # data provided
 tmp = os.getenv("TMP", "/tmp")
@@ -14,6 +16,25 @@ urllib.request.urlretrieve(
     harry_text
 )
 
+with open(stopwords_file) as f:
+    stop_words = f.read().lower().split()
+    # print(stop_words)
+
+with open(harry_text, 'r', encoding='utf-8') as f:
+    text = f.read().lower().split()
+    # print(text)
+
 
 def get_harry_most_common_word():
-    pass
+
+    no_stop_words = [word for word in text if word not in stop_words]
+
+    isalnum_chars = [char for char in no_stop_words if char.isalnum()]
+
+    # counter = Counter(isalnum_chars).most_common()[0]
+    most_common_word = Counter(isalnum_chars).most_common()[0][0]
+    frequency = Counter(isalnum_chars).most_common()[0][1]
+    print(most_common_word, frequency)
+
+
+get_harry_most_common_word()
