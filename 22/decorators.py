976a6765cba6e7787354a8a8373c0ad1fc00beab
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-08-26 09:09:00 +0200

    submission Bite 22 @ codechalleng.es

--- 22/decorators.py
+++ 22/decorators.py
@@ -2,4 +2,9 @@ from functools import wraps
 
 
 def make_html(element):
-    pass
+    def tag_decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            return f"<{element}>" + fn(*args, **kwargs) + f"</{element}>"
+        return wrapper
+    return tag_decorator
