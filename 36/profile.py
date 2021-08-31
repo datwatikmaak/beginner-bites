57d391ea172caca56d63ec181ea0721964817906
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-08-31 05:44:22 +0200

    submission Bite 36 @ codechalleng.es

--- 36/profile.py
+++ 36/profile.py
@@ -1,2 +1,14 @@
-def get_profile():
-    pass
+def get_profile(name: str, age: int, *args, **kwargs):
+    if type(age) != int or len(args) > 5:
+        raise ValueError
+
+    if len(args) > 1:
+        args = (sorted(args))
+
+    result = {"name": name, "age": age}
+    if args:
+        result["sports"] = sorted(args)
+    if kwargs:
+        result["awards"] = kwargs
+
+    return result
